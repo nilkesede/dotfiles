72b1d93188962499c2639ea38a63ea74ffbbb11d
Nil Késede <nilkesede@gmail.com>
2020-05-31 14:46:24 -0300

    update config.fish

--- config.fish
+++ config.fish
@@ -1,9 +1,11 @@
-# set -g theme_display_docker_machine no
+set -g theme_display_docker_machine no
 
-set -Up fish_user_paths /sbin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin
+set -xp PATH /sbin /usr/sbin /usr/local/bin /usr/local/sbin 
 
 set -x LANG en_US.UTF-8
 set -x NVM_DIR /usr/local/opt/nvm
 
 alias g="git"
 alias l="ls -lah"
+alias cl="clear"
+alias o="open ."
